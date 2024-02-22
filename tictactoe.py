735f9b51aa7488e0a5b98b6992f4d208e0b5474a
Vincent Tran <vincent.tran895@gmail.com>
2024-02-21 19:08:41 -0800

    Added a bunch of stuff
    Everything besides results and minmax

--- tictactoe.py
+++ tictactoe.py
@@ -22,43 +22,93 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    #If X is equal to the amount of O's, it is X's turn
+    X_num = sum(row.count(X) for row in board)
+    O_num = sum(row.count(O) for row in board)
+
+    if X_num <= O_num:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
-
+    actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                actions.add((i,j))
+    return actions
+    
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
-
+    #Check horizontally
+    for row in board:
+        if row[0] == row[1] == row[2]:
+            if row[0] is not None:
+                return row[0]
+        
+    #Checks vertically
+    for col in range(3):
+        if board[col][0] == board[col][1] == board[col][2]:
+            if board[col][0] is not None:
+                return board[col][0]
+    
+    #Checks diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+    
+    #If tie/no three in a row
+    return None
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
-
+    #Three in a row is found
+    if winner(board) == True:
+        return True
+    
+    #There is still an empty spot
+    for row in board:
+        if EMPTY in row:
+            return False
+    
+    #Will return True if all rows are filled
+    return True
+    
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
 
+    #If X wonw
+    if winner(board) == X:
+        return 1
+    
+    #If O won
+    elif winner(board) == O:
+        return -1
+
+    #If tie
+    else: 
+        return 0
 
 def minimax(board):
     """
